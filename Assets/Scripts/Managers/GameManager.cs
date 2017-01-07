f8a646cb8e0bfc7f0d429c1b8e9b4a12ba477161
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2017-01-07 08:44:26 -0500

    Updated TreePrefab

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,11 +4,15 @@ public enum GM_InGame_State { Playing, Starting, Paused ,Ending}
 public enum GM_Nature_State { Equilibrium, LowNatureLevel, HighNatureLevel }
 public class GameManager : MonoBehaviour
 {
+    public float debugNatureLevel=0.5f;
     public static GameManager instance = null;
     public float gameTimer = 0;
     public float equilibriumRange = .2f;
     public GM_InGame_State state;
     public const float EQUILIBRIUM_LEVEL=0.5F;
+
+    private float deltaNatureLevel;
+    private GM_Nature_State deltaNatureState;
     // Use this for initialization
     void Awake()
     {
@@ -22,6 +26,8 @@ public class GameManager : MonoBehaviour
         }
         state = GM_InGame_State.Starting;
 
+        StartGame();
+
     }
 
     // Update is called once per frame
@@ -29,13 +35,42 @@ public class GameManager : MonoBehaviour
     {
         if (state == GM_InGame_State.Playing)
         {
+            //Was there a change in the nature level
+            if(deltaNatureLevel != GetNatureLevel())
+            {
+                deltaNatureLevel = GetNatureLevel();
+                if (GetNatureState()== GM_Nature_State.HighNatureLevel)//Trees are sickly, update them
+                {
+                    StartCoroutine("TurnAllTreesSickly");
+                }
+                else if(deltaNatureState == GM_Nature_State.HighNatureLevel)//Trees were sickly but now arent, update them
+                {
+                    StartCoroutine("TurnAllTreesHealthy");
+                }
+            }
             gameTimer += Time.deltaTime;
         }
-
+        
+    }
+    IEnumerator TurnAllTreesHealthy()
+    {
+        foreach (TreeComponent tree in FindObjectsOfType<TreeComponent>())
+        {
+            tree.TurnHealthy();
+        }
+        return null;
+    }
+    IEnumerator TurnAllTreesSickly()
+    {
+        foreach (TreeComponent tree in FindObjectsOfType<TreeComponent>())
+        {
+            tree.TurnSickly();
+        }
+        return null;
     }
     public float GetNatureLevel()
     {
-        return 0.5f;
+        return debugNatureLevel;
     }
     public GM_Nature_State GetNatureState()
     {
@@ -43,7 +78,7 @@ public class GameManager : MonoBehaviour
         {
             return GM_Nature_State.LowNatureLevel;
         }
-        if(GetNatureLevel() <= EQUILIBRIUM_LEVEL - (equilibriumRange / 2))
+        if(GetNatureLevel() >= EQUILIBRIUM_LEVEL + (equilibriumRange / 2))
         {
             return GM_Nature_State.HighNatureLevel;
         }
--- Assets/Scripts/Tiles/TreeComponent.cs
+++ Assets/Scripts/Tiles/TreeComponent.cs
@@ -1,36 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(MeshRenderer))]
+//Tree class that handles all tree states and behaviours
+/*
+ * Call Spawn() when planting a tree
+ * Call ReceiveDisease() to contaminate a tree
+ */
+
 public class TreeComponent : MonoBehaviour
 {
-
-    public Color diseasedColor;
-    public Color sicklyColor;
+    public Color diseasedLeavesColor;
+    public Color sicklyLeavesColor;
+    public Color diseasedBarkColor;
+    public Color sicklyBarkColor;
     public float TimeToTurnDiseased=2;
 
-    Color healthyColor;
-    MeshRenderer mr;
+    Color healthyBarkColor;
+    Color healthyLeavesColor;
+    SkinnedMeshRenderer mr;
+    Animator anim;
     bool isDiseased = false;
+    bool isSickly = false;
+    const int LEAVES_MAT_INDEX = 0;
+    const int BARK_MAT_INDEX = 1;
     // Use this for initialization
     void Start()
     {
-        mr = GetComponent<MeshRenderer>();
-        healthyColor = mr.material.color;
+        mr = GetComponentInChildren<SkinnedMeshRenderer>();
+        anim = GetComponent<Animator>();
+        healthyBarkColor = mr.materials[BARK_MAT_INDEX].color;
+        healthyLeavesColor = mr.materials[LEAVES_MAT_INDEX].color;
     }
-
-    // Update is called once per frame
-    void Update()
+    void Spawn()
     {
-        EvaluateHealth();
-    }
-    void EvaluateHealth()
-    {
-        if(GameManager.instance.GetNatureState()==GM_Nature_State.HighNatureLevel)
-        {
-            Color newColor = Color.Lerp(healthyColor, sicklyColor, (GameManager.instance.GetNatureLevel()-0.5f)/0.5f);
-            mr.material.color = newColor;
-        }
+        anim.SetTrigger("PlantTree");
     }
     void Die()
     {
@@ -43,20 +46,45 @@ public class TreeComponent : MonoBehaviour
     public void ReceiveDisease()
     {
         isDiseased = true;
+        anim.SetBool("Diseased", true);
         StartCoroutine(TurnToDiseased());
     }
     private void SpreadDisease()
     {
 
     }
+    public void TurnSickly()
+    {
+        Color newColor = Color.Lerp(healthyBarkColor, sicklyBarkColor, (GameManager.instance.GetNatureLevel() - 0.5f) / 0.5f);
+        mr.materials[BARK_MAT_INDEX].color = newColor;
+
+        newColor = Color.Lerp(healthyLeavesColor, sicklyLeavesColor, (GameManager.instance.GetNatureLevel() - 0.5f) / 0.5f);
+        mr.materials[LEAVES_MAT_INDEX].color = newColor;
+        isSickly = true;
+        
+    }
+    public void TurnHealthy()
+    {
+        Color newColor = healthyBarkColor;
+        mr.materials[BARK_MAT_INDEX].color = newColor;
+
+        newColor = healthyLeavesColor;
+        mr.materials[LEAVES_MAT_INDEX].color = newColor;
+        isSickly = false;
+
+    }
     IEnumerator TurnToDiseased()
     {
-        for (int i = 0; i < 10; i++)
+
+        for (int i = 0; i < 20; i++)
         {
-            Color newColor = Color.Lerp(mr.material.color, diseasedColor, 0.1f);
-            mr.material.color = newColor;
-            yield return new WaitForSeconds(TimeToTurnDiseased/10);
+            Color newColor = Color.Lerp(mr.materials[BARK_MAT_INDEX].color, diseasedBarkColor, i/20.0f);
+            mr.materials[BARK_MAT_INDEX].color = newColor;
+            newColor = Color.Lerp(mr.materials[LEAVES_MAT_INDEX].color, diseasedLeavesColor, i/20.0f);
+            mr.materials[LEAVES_MAT_INDEX].color = newColor;
+            yield return new WaitForSeconds(TimeToTurnDiseased/20);
         }
+        SpreadDisease();
 
     }
 
