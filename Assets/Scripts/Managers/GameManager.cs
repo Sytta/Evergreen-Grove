443f4e76ced56b7fb6995fecb627a3030594eec7
unknown <nadjam@326-D3.nad.qc.ca>
2017-01-07 12:41:13 -0500

    fix

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -107,12 +107,16 @@ public class GameManager : MonoBehaviour
 
         terrainManager.Initialise();
         terrainManager.GenerateGrid();
+        
+        Debug.Log(terrainManager.WorldPosToGridPos(new Vector3(-11, 0, -12)));
+        Debug.Log(terrainManager.WorldPosToGridPos(new Vector3(-2, 0, -14)));
+        Debug.Log(terrainManager.WorldPosToGridPos(new Vector3(14.4f, 0, -6)));
 
         yield return new WaitForSeconds(2);
 
         // This will eventually become a loop where either of those two are called once in a while
         // This loop will be placed in the EventManager.
         //terrainManager.AddRandomSeed();
-        terrainManager.InfectRandomTree();
+        //terrainManager.InfectRandomTree();
     }
 }
