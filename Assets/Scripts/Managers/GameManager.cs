f16c83edf3be34fc8ba6023e224dc10a499bcbca
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2017-01-06 23:20:37 -0500

    Added managers

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum InGame_State { Playing,Starting,Paused}
+public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+    public float gameTimer=0;
+    public InGame_State state;
+    // Use this for initialization
+    void Awake () {
+	    if(!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        state = InGame_State.Starting;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(state==InGame_State.Playing)
+        {
+            gameTimer += Time.deltaTime;
+        }
+	
+	}
+    public float GetNatureLevel()
+    {
+        return 0.5f;
+    }
+}
