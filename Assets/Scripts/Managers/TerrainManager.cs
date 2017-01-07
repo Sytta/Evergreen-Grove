e17cdf03ede111fee87cb0b4f06298a1e706a902
unknown <nadjam@326-D1.nad.qc.ca>
2017-01-07 13:44:14 -0500

    added tags

--- Assets/Scripts/Managers/TerrainManager.cs
+++ Assets/Scripts/Managers/TerrainManager.cs
@@ -222,7 +222,7 @@ public class TerrainManager : MonoBehaviour
     }
 
     // For debugging purposes, showing the grid.
-    /*void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
 
@@ -238,7 +238,7 @@ public class TerrainManager : MonoBehaviour
                 }
             }
         }
-    }*/
+    }
 
     // Returns a float between 0 and 1 which tells us where on the equilibrium bar we are now
     public float GetNatureLevel()
