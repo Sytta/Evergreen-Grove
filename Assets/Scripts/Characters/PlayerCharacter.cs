0c515250af633325edc124d0d4bb54f68a6a7614
unknown <nadjam@326-D3.nad.qc.ca>
2017-01-07 22:19:15 -0500

    Wisp movement fixed

--- Assets/Scripts/Characters/PlayerCharacter.cs
+++ Assets/Scripts/Characters/PlayerCharacter.cs
@@ -21,8 +21,8 @@ public abstract class PlayerCharacter : MonoBehaviour {
 	
 	// Update is called once per frame
 	virtual protected void Update () {
-        horAxis = Input.GetAxis(characterName+"Horizontal");
-        verAxis = Input.GetAxis(characterName+"Vertical");
+        verAxis = -Input.GetAxis(characterName+"Horizontal");
+        horAxis = -Input.GetAxis(characterName+"Vertical");
         ManagePlayerModelRotation();
         if (Mathf.Abs(horAxis) > 0 || Mathf.Abs(verAxis) > 0)
         {
