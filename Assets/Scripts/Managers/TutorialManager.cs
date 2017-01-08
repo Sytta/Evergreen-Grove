e1097e985b7bd552f0cf313e764ed20aa03b80d5
unknown <nadjam@326-C5.nad.qc.ca>
2017-01-07 23:03:34 -0500

    mainmenu changes

---
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour {
+
+    bool tutorial_on;
+
+    public void On_Toggle()
+    {
+        GameManager.instance.isTutorialMode = true;
+    }
+}
+    
