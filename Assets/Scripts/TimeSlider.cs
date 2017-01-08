0a802410be3d99750055be0f84e520862ac3a005
unknown <nadjam@326-C3.nad.qc.ca>
2017-01-08 12:41:17 -0500

    Changing time slider

---
+++ Assets/Scripts/TimeSlider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeSlider : MonoBehaviour {
+
+    public Sprite Sunset;
+    public Sprite MidDay;
+    public Sprite Moon; 
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
