a18397792a7b65141f0d7c78781098d1580f072a
unknown <nadjam@326-C5.nad.qc.ca>
2017-01-07 21:19:40 -0500

    Changed EventManager a lot. more stable and no EXTREME MODE because it helps player

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
     public float gameTimer = 0;
-    public float equilibriumRange = .2f;
+    public float equilibriumRange = .01f;
     public int seeds=0;
     public TerrainManager terrainManager;
     public GM_InGame_State state;
