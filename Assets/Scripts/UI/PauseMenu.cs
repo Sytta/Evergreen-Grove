c86a89b7ab79b662f9ef36938f2e8bf174dadf9b
unknown <nadjam@326-D3.nad.qc.ca>
2017-01-07 23:00:03 -0500

    Wisp has its commands now

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -30,7 +30,7 @@ public class PauseMenu : MonoBehaviour {
         {
             EnableMenu();
         }
-        else if (Input.anyKeyDown)
+        else if (GameManager.instance.state == GM_InGame_State.Paused && Input.anyKeyDown)
         {
             GameManager.instance.UnPauseGame();
             DisableMenu();
